14bc38dc3ae3f79d4df233418b196fde61317d2a
Thomas Vargiu <thomas.vargiu@facile.it>
2018-04-18 23:42:27 +0200

    Added ZF classes

---
+++ src/ConfigProvider.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace Facile\DoctrineDDM;
+
+use Facile\DoctrineDDM\Factory\MetadataConfigFactory;
+use Facile\DoctrineDDM\Factory\MetadataListenerFactory;
+use Facile\DoctrineDDM\Configuration\Metadata;
+
+class ConfigProvider
+{
+    public function __invoke()
+    {
+        return [
+            'dependencies' => $this->getDependencyConfig(),
+        ];
+    }
+
+    /**
+     * Return default service mappings for zend-cache.
+     *
+     * @return array
+     */
+    public function getDependencyConfig(): array
+    {
+        return [
+            'factories' => [
+                // register the configuration factory
+                Metadata::class => MetadataConfigFactory::class,
+                // register the metadata listener factory
+                MetadataListener::class => MetadataListenerFactory::class,
+            ],
+        ];
+    }
+}
---
+++ src/Module.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Facile\DoctrineDDM;
+
+class Module
+{
+    /**
+     * @return array
+     */
+    public function getConfig()
+    {
+        $provider = new ConfigProvider();
+
+        return [
+            'service_manager' => $provider->getDependencyConfig(),
+        ];
+    }
+}
